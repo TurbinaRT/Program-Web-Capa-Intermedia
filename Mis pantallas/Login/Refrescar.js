86438f887c1b1320f08c69893ad5046f7710d848
TurbinaRT <gabriela.vazquezua@uanl.edu.mx>
2023-03-30 17:40:20 -0600

    Cambios pequeños
    a

---
+++ Mis pantallas/Login/Refrescar.js
@@ -0,0 +1,3 @@
+setInterval(function() {
+    $('.carousel-item ').load('index.html'); // Selector de la div y el fichero a refrescar
+  }, 2000); // Temporizador que ejecuta el refresco cada 2 segundos
